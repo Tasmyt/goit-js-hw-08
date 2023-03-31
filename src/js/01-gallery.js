15915b4be264bce5c664f329656453f5c4918c23
Tasmyt <121162710+Tasmyt@users.noreply.github.com>
2023-03-31 17:14:08 +0300

    31/03

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const ListImage = document.querySelector(".gallery");
+const cardImages = galleryCardsMarkup(galleryItems);
+ListImage.insertAdjacentHTML("beforeend", cardImages);
+
+function galleryCardsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+        <a class='gallery__link' href = '${original}'>
+            <img
+                class="gallery__image"
+                src= "${preview}" 
+                alt = '${description}' /> 
+        </a>
+        </li>`;
+    })
+    .join("");
+}
+
+
+new SimpleLightbox(".gallery__item a", {
+captionsData: "alt",
+captionPosition: "bottom",
+captionDelay: 250,
+});
 
 console.log(galleryItems);
