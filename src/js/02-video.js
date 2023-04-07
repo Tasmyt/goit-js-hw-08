a784352dc5afcd311040d71e4fc5f8ed031866e6
Tasmyt <121162710+Tasmyt@users.noreply.github.com>
2023-04-07 15:26:08 +0300

    7/04

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+console.log(Player);
+const CURRENT_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+const onTimeupdate = function(data) {
+    const seconds = data.seconds;
+    console.log(seconds);
+    localStorage.setItem(CURRENT_KEY, JSON.stringify(seconds));
+};
+
+player.on('timeupdate', throttle(onTimeupdate, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_KEY)));
